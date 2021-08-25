b7c991c7336c0c820e446c737c3e913550a2659c
bhaveshlokhande-000 <72037462+bhaveshlokhande-000@users.noreply.github.com>
2021-08-25 14:55:03 +0530

    Update signOut.js

--- nodeJS/users/signOut.js
+++ nodeJS/users/signOut.js
@@ -3,7 +3,6 @@ const firebase = require("../services/userServices").getInstance()
 module.exports = function (req, res) {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
-        console.log("signout", req.query)
         res.status(200).send({
             message: "signOut",
         })
